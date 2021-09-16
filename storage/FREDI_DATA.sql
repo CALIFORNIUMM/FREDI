a21a0e04997a5db550ff0548ae6d4e7f394e8e8e
CALIFORNIUMM <yohan.marques@limayrac.fr>
2021-09-16 16:12:40 +0200

    ui

--- storage/FREDI_DATA.sql
+++ storage/FREDI_DATA.sql
@@ -1,1 +1,24 @@
-INSERT INTO
+INSERT INTO ligues(`id_ligue`,`nom`)
+VALUES
+(1,"Ligue de Judo Auvergne-Rhone-Alpes"),
+(2,"Ligue de football Haute-Garonne"),
+(3,"Ligue de football d'Aquitaine"),
+(4,"Ligue de tennis de Corse du Sud"),
+(5,"Ligue d'equitation du VAR"),
+(6,"Ligue de natation du Quercy");
+
+INSERT INTO clubs('id_club', 'nom', 'cp' ,'numero_rue', 'nom_rue', 'ville', 'id_ligue')
+VALUES
+(1,'Dojo Burgien','1 rue du Docteur DUBY','?','1000',1),
+(2,'Dojo Burgien','1 rue du Docteur DUBY','?','1000',1),
+(3,'Dojo Burgien','1 rue du Docteur DUBY','?','1000',1),
+(4,'Dojo Burgien','1 rue du Docteur DUBY','?','1000',1),
+(5,'Dojo Burgien','1 rue du Docteur DUBY','?','1000',1),
+(6,'Dojo Burgien','1 rue du Docteur DUBY','?','1000',1),
+(7,'Dojo Burgien','1 rue du Docteur DUBY','?','1000',1),
+(8,'Dojo Burgien','1 rue du Docteur DUBY','?','1000',1),
+(9,'Dojo Burgien','1 rue du Docteur DUBY','?','1000',1),
+(10,'Dojo Burgien','1 rue du Docteur DUBY','?','1000',1),
+(11,'Dojo Burgien','1 rue du Docteur DUBY','?','1000',1),
+(12,'Dojo Burgien','1 rue du Docteur DUBY','?','1000',1)
+;
