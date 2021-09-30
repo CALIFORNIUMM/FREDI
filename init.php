b715ac873b1396632cf8b4b846454c37efcc2546
david <david.peyrard@limayrac.fr>
2021-09-30 14:01:54 +0200

    csv

--- init.php
+++ init.php
@@ -8,8 +8,18 @@
 /**
  * Paramétrage pour certains serveurs qui n'affichent pas les erreurs PHP par défaut
  */
+error_reporting(E_ALL | E_STRICT);
 ini_set('display_errors', '1');
 ini_set('html_errors', '1');
+// Autorise les uploads de fichier
+ini_set('file_uploads', '1');
+// Encodage avec les fonctions mb_*
+mb_internal_encoding('UTF-8');
+// Force le fuseau de Paris
+date_default_timezone_set('Europe/Paris');
+// Chemins dans l'OS
+define('DS', DIRECTORY_SEPARATOR);   // Séparateur des noms de dossier (dépend de l'OS)
+define('ROOT', dirname(__FILE__));  // Racine du site en absolu (à utiliser dans les includes par exemple)
 
 /**
  * Autoload
@@ -44,3 +54,33 @@ header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
  define('DB_PASSWORD','Limayrac#31');
  define('DB_HOST','localhost');
  define('DB_NAME','fredi21');
+ 
+/**
+ * Chargement CSV
+ */
+ function load_from_csv(string $filename, int $start = 1)
+    {
+        // Ouverture du fichier
+        $file_handler = fopen($filename, "r") or exit("<p>Impossible de lire le fichier $filename</p>");
+        $nb = 1;
+        $rows = array();
+        // Boucle de lecture
+        while (!feof($file_handler)) {
+            $row = fgetcsv($file_handler, 0, ';');
+            if ($nb >= $start) {
+            $rows[] = $row;
+            }
+            $nb++;
+        }
+        // Fermeture du fichier
+        fclose($file_handler);
+        // Renvoie le tableau PHP
+        return $rows;
+    }
+
+    function convertir_date(string $date1)
+    {
+    $datetime = DateTime::createFromFormat('d/m/Y', $date1, new DateTimeZone("Europe/Paris"));
+    $date2 = $datetime->format("Y-m-d");
+    return $date2;
+    }
