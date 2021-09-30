a1a8fbf7c3527298351c177a2ec50829a5c5c039
QuinteroAgustin <agustin.quintero@limayrac.fr>
2021-09-30 13:53:14 +0200

    dao2

--- app/dao/UserDAO.php
+++ app/dao/UserDAO.php
@@ -39,5 +39,58 @@
             // Retourne un tableau d'objets "salarié"
             return $user;
         } // function findAll()
+
+        public function newUser(User $user){
+            $sql="INSERT INTO utilisateur(pseudo, mdp, mail, nom, prenom, role) VALUES (:pseudo, :mdp, :mail, :nom, :prenom, 1)";
+            $request=$this->dbh->prepare($sql);
+            $request->execute(array(
+                ":pseudo" => $user->get_pseudo(),
+                ":mdp" => $user->get_mdp(),
+                ":mail" => $user->get_mail(),
+                ":nom" => $user->get_nom(),
+                ":prenom" => $user->get_prenom(),
+            ));
+        }
+    
+        public function isExistPseudo($pseudo){
+            $sql="SELECT count(*) as nb FROM utilisateur WHERE pseudo = :pseudo";
+            $request=$this->dbh->prepare($sql);
+            $request->execute(array(
+                ":pseudo" => $pseudo
+            ));
+            $response = $request->fetch();
+            if($response['nb'] == 0){
+                return False;
+            }else{
+                return True;
+            };
+        }
+    
+        public function isExistMail($mail){
+            $sql="SELECT count(*) as nb FROM utilisateur WHERE mail = :mail";
+            $request=$this->dbh->prepare($sql);
+            $request->execute(array(
+                ":mail" => $mail
+            ));
+            $response = $request->fetch();
+            if($response['nb'] == 0){
+                return False;
+            }else{
+                return True;
+            };
+        }
+    
+        function connexionUser($pseudo){
+            $sql="SELECT id_utilisateur, mdp FROM utilisateur WHERE pseudo = :pseudo";
+            $request=$this->dbh->prepare($sql);
+            $request->execute(array(
+                ":pseudo" => $pseudo
+            ));
+            $response = $request->fetch();
+            return $response;
+        }
+
+
+
     }
 ?>
