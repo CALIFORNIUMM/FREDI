ab67e895ce24e05e3bf6ce9361375837ab513817
PHAkendengue <pierrehonore.akendengue@limayrac.fr>
2021-10-07 15:14:54 +0200

    Liste des utilisateurs

---
+++ utilisateur_liste.php
@@ -0,0 +1,42 @@
+
+<?php
+  include('header.php');
+  $title = "Utilisateur";
+
+  //chargement de la classe ligues
+  $users = new UserDAO();
+  $users = $users->findAll();
+
+?>
+  <h1>Admin</h1>
+  <h2>Liste des Utilisateur</h2>
+  <ul>
+    <li><a href="motifs_charger.php">Charger motifs</a></li>
+    <li><a href="motifs_liste.php">Liste motifs</a></li>
+    <li><a href="clubs_charger.php">Charger clubs</a></li>
+    <li><a href="clubs_liste.php">Liste clubs</a></li>
+    <li><a href="ligues_charger.php">Charger ligues</a></li>
+    <li><a href="ligues_liste.php">Liste ligues</a></li>
+  </ul>
+
+  <?php
+  if (count($ligues) > 0) {
+  ?>
+    <table>
+      <tr>
+        <th>Pseudo</th>
+      </tr>
+      <?php
+      foreach ($users as $user) {
+        echo '<tr>';
+        echo '<td>' . $user->get_pseudo(). '</td>';
+        echo "</tr>";
+      } ?>
+    </table>
+<?php
+} else {
+  echo "<p>Rien à afficher</p>";
+}
+echo "<p>". count($ligues) ." Utilisateur(s)</p>";
+?>
+<?php include('footer.php'); ?>
