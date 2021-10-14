f4b69fa45e46939c9da85d27dd659789d18aab1c
Quintero Agustin <agustin.quintero@limayrac.fr>
2021-10-14 16:31:54 +0200

    data

--- storage/sql/__fredi21_structure.sql
+++ storage/sql/__fredi21_structure.sql
@@ -38,16 +38,6 @@ CREATE TABLE `adherent` (
   `id_club` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Déchargement des données de la table `adherent`
---
-
-INSERT INTO `adherent` (`id_adherent`, `nr_licence`, `adr1`, `adr2`, `adr3`, `id_utilisateur`, `id_club`) VALUES
-(1, '123', '1 rue du clone', '31000', 'toulouse', 9, 11),
-(2, '012', '2 rue du clone', '31000', 'toulouse', 10, 5),
-(3, '456', '3 rue du clone', '31000', 'toulouse', 11, 6),
-(4, '789', '4 rue du clone', '31000', 'toulouse', 12, 12);
-
 -- --------------------------------------------------------
 
 --
@@ -62,24 +52,6 @@ CREATE TABLE `club` (
   `adr3` varchar(50) DEFAULT NULL,
   `id_ligue` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Déchargement des données de la table `club`
---
-
-INSERT INTO `club` (`id_club`, `lib_club`, `adr1`, `adr2`, `adr3`, `id_ligue`) VALUES
-(1, 'Dojo Burgien', '1 rue du Docteur DUBY', '1000', 'BOURG EN BRESSE', 1),
-(2, 'Saint-Denis Dojo', '239 Allées des sports', '1000', 'ST DENIS LES BOURG', 1),
-(3, 'Judo Club Vallée Arbent', 'rue du Général ANDREA', '1100', 'ARBENT', 1),
-(4, 'Belli Judo', '1 rue du Bac', '1100', 'BELLIGNAT', 1),
-(5, 'Racing Club Montluel Judo', '170 rue des Chartinières', '1120', 'DAGNEUX', 1),
-(6, 'Centre Arts Martiaux Pondinois', 'rue de l Oiselon', '1160', 'PONT D AIN', 1),
-(7, 'Judo Club Ornex', '58 rue des Pralets', '1210', 'ORNEX', 1),
-(8, 'Dojo Gessien Valserine', '58 rue des Pralets', '1220', 'DIVONNE LES BAINS', 1),
-(9, 'Dojo La Vallière', 'Complexe Sportif', '1250', 'MONTAGNAT', 1),
-(10, 'Football club Merville', 'Rue Emile Pouvillon', '31330', 'MERVILLE', 2),
-(11, 'Football Club Bassin d Arcachon', 'Boulevard Mestrezat - Stade jean Brousse', '33120', 'ARCACHON', 3),
-(12, 'Andernos Sport Football Club', 'Plaine des Sports Jacques Rosazza', '33510', 'ANDERNOS LES BAINS', 3);
 
 -- --------------------------------------------------------
 
@@ -112,18 +84,6 @@ CREATE TABLE `ligue` (
   `lib_ligue` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Déchargement des données de la table `ligue`
---
-
-INSERT INTO `ligue` (`id_ligue`, `lib_ligue`) VALUES
-(1, 'Ligue de Judo Auvergne-Rhone-Alpes'),
-(2, 'Ligue de football Haute-Garonne'),
-(3, 'Ligue de football d\'Aquitaine'),
-(4, 'Ligue de tennis de Corse du Sud'),
-(5, 'Ligue d\'equitation du VAR'),
-(6, 'Ligue de natation du Quercy');
-
 -- --------------------------------------------------------
 
 --
@@ -135,20 +95,6 @@ CREATE TABLE `motif` (
   `lib_motif` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Déchargement des données de la table `motif`
---
-
-INSERT INTO `motif` (`id_motif`, `lib_motif`) VALUES
-(1, 'Réunion'),
-(2, 'Compétition régionale'),
-(3, 'Compétition nationale'),
-(4, 'Compétition internationnale'),
-(5, 'Stage'),
-(6, 'Visite médicale'),
-(7, 'Oxygénation'),
-(8, 'Convocation'),
-(9, 'Formation');
 
 -- --------------------------------------------------------
 
@@ -195,19 +141,6 @@ CREATE TABLE `utilisateur` (
   `role` smallint(6) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Déchargement des données de la table `utilisateur`
---
-
-INSERT INTO `utilisateur` (`id_utilisateur`, `pseudo`, `mdp`, `mail`, `nom`, `prenom`, `role`) VALUES
-(9, 'yohan', '$2y$10$jaa7NnbdBhEuL5hMLGMbteg.KXzsep5/VCVs.Ql9DZj/SYZ45jHdG', 'yohan.marques@limayrac.fr', 'marques', 'yohan', 2),
-(10, 'agustin', '$2y$10$ZT6T366PoTdRCNY.8KCYaeL5atqndjRct0ToNsksraYWlRWYC2qa2', 'agustin.quintero@limayrac.fr', 'Quintero', 'Agustin', 2),
-(11, 'david', '$2y$10$18jqscpcfns8YHYcYFabl.dIFP7pmLptXd4WTm4WpwgBtQqBsguym', 'david.peyrard@limayrac.fr', 'Peyrard', 'David', 1),
-(12, 'ph', '$2y$10$50fd6ZGE.4gSJmwGCubcLeMzzx6e76j6AU5NzFWOtwF77HQCF.OC6', 'pierrehonore.akendengue@limayrac.fr', 'Akendengue', 'Pierre-Honoré', 0);
-
---
--- Index pour les tables déchargées
---
 
 --
 -- Index pour la table `adherent`
