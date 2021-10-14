8ce8e90f7413cb434193cc62d63714d103c6085c
Quintero Agustin <agustin.quintero@limayrac.fr>
2021-10-14 14:18:32 +0200

    csv foreign key

--- storage/sql/club.sql
+++ storage/sql/club.sql
@@ -1,72 +1,17 @@
---
--- Structure de la table `adherent`
---
-DROP TABLE IF EXISTS `adherent`;
---
--- Structure de la table `club`
---
-DROP TABLE IF EXISTS `club`;
---
--- Structure de la table `club`
---
+SET FOREIGN_KEY_CHECKS = 0;
+USE fredi21;
+DROP TABLE IF EXISTS club;
+
 CREATE TABLE `club` (
-  `id_club` int(11) NOT NULL,
-  `lib_club` varchar(50) DEFAULT NULL,
-  `adr1` varchar(50) DEFAULT NULL,
-  `adr2` varchar(50) DEFAULT NULL,
-  `adr3` varchar(50) DEFAULT NULL,
-  `id_ligue` int(11) NOT NULL
+`id_club` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+`lib_club` varchar(50) DEFAULT NULL,
+`adr1` varchar(50) DEFAULT NULL,
+`adr2` varchar(50) DEFAULT NULL,
+`adr3` varchar(50) DEFAULT NULL,
+`id_ligue` int(11) NOT NULL,
+CONSTRAINT `fk_id_ligue` FOREIGN KEY (`id_ligue`) REFERENCES `ligue` (`id_ligue`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Index pour la table `club`
---
-ALTER TABLE `club`
-  ADD PRIMARY KEY (`id_club`),
-  ADD KEY `fk_id_ligue` (`id_ligue`);
 
---
--- AUTO_INCREMENT pour la table `club`
---
-ALTER TABLE `club`
-  MODIFY `id_club` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
---
--- Contraintes pour la table `club`
---
-ALTER TABLE `club`
-  ADD CONSTRAINT `fk_id_ligue` FOREIGN KEY (`id_ligue`) REFERENCES `ligue` (`id_ligue`);
-
---
--- Structure de la table `adherent`
---
-CREATE TABLE `adherent` (
-  `id_adherent` int(11) NOT NULL,
-  `nr_licence` varchar(50) DEFAULT NULL,
-  `adr1` varchar(50) DEFAULT NULL,
-  `adr2` varchar(50) DEFAULT NULL,
-  `adr3` varchar(50) DEFAULT NULL,
-  `id_utilisateur` int(11) NOT NULL,
-  `id_club` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Index pour la table `adherent`
---
-ALTER TABLE `adherent`
-  ADD PRIMARY KEY (`id_adherent`),
-  ADD KEY `fk_id_utilisateur2` (`id_utilisateur`),
-  ADD KEY `fk_id_club` (`id_club`);
-
---
--- AUTO_INCREMENT pour la table `adherent`
---
-ALTER TABLE `adherent`
-  MODIFY `id_adherent` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- Contraintes pour la table `adherent`
---
-ALTER TABLE `adherent`
-  ADD CONSTRAINT `fk_id_club` FOREIGN KEY (`id_club`) REFERENCES `club` (`id_club`),
-  ADD CONSTRAINT `fk_id_utilisateur2` FOREIGN KEY (`id_utilisateur`) REFERENCES `utilisateur` (`id_utilisateur`);
+SET FOREIGN_KEY_CHECKS = 1;
--- storage/sql/ligue.sql
+++ storage/sql/ligue.sql
@@ -1,93 +1,13 @@
---
--- Structure de la table `adherent`
---
-DROP TABLE IF EXISTS `adherent`;
---
--- Structure de la table `club`
---
-DROP TABLE IF EXISTS `club`;
---
--- Structure de la table `ligue`
---
-DROP TABLE IF EXISTS `ligue`;
+SET FOREIGN_KEY_CHECKS = 0;
+USE fredi21;
+DROP TABLE IF EXISTS ligue;
+
 CREATE TABLE `ligue` (
-  `id_ligue` int(11) NOT NULL,
+  `id_ligue` int(11) NOT NULL PRIMARY KEY,
   `lib_ligue` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Index pour la table `ligue`
---
 ALTER TABLE `ligue`
-  ADD PRIMARY KEY (`id_ligue`);
+  MODIFY `id_ligue` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT pour la table `ligue`
---
-ALTER TABLE `ligue`
-  MODIFY `id_ligue` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-
---
--- Structure de la table `club`
---
-CREATE TABLE `club` (
-  `id_club` int(11) NOT NULL,
-  `lib_club` varchar(50) DEFAULT NULL,
-  `adr1` varchar(50) DEFAULT NULL,
-  `adr2` varchar(50) DEFAULT NULL,
-  `adr3` varchar(50) DEFAULT NULL,
-  `id_ligue` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Index pour la table `club`
---
-ALTER TABLE `club`
-  ADD PRIMARY KEY (`id_club`),
-  ADD KEY `fk_id_ligue` (`id_ligue`);
-
---
--- AUTO_INCREMENT pour la table `club`
---
-ALTER TABLE `club`
-  MODIFY `id_club` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
-
---
--- Contraintes pour la table `club`
---
-ALTER TABLE `club`
-  ADD CONSTRAINT `fk_id_ligue` FOREIGN KEY (`id_ligue`) REFERENCES `ligue` (`id_ligue`);
-
---
--- Structure de la table `adherent`
---
-CREATE TABLE `adherent` (
-  `id_adherent` int(11) NOT NULL,
-  `nr_licence` varchar(50) DEFAULT NULL,
-  `adr1` varchar(50) DEFAULT NULL,
-  `adr2` varchar(50) DEFAULT NULL,
-  `adr3` varchar(50) DEFAULT NULL,
-  `id_utilisateur` int(11) NOT NULL,
-  `id_club` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Index pour la table `adherent`
---
-ALTER TABLE `adherent`
-  ADD PRIMARY KEY (`id_adherent`),
-  ADD KEY `fk_id_utilisateur2` (`id_utilisateur`),
-  ADD KEY `fk_id_club` (`id_club`);
-
---
--- AUTO_INCREMENT pour la table `adherent`
---
-ALTER TABLE `adherent`
-  MODIFY `id_adherent` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- Contraintes pour la table `adherent`
---
-ALTER TABLE `adherent`
-  ADD CONSTRAINT `fk_id_club` FOREIGN KEY (`id_club`) REFERENCES `club` (`id_club`),
-  ADD CONSTRAINT `fk_id_utilisateur2` FOREIGN KEY (`id_utilisateur`) REFERENCES `utilisateur` (`id_utilisateur`);
+SET FOREIGN_KEY_CHECKS = 1;
