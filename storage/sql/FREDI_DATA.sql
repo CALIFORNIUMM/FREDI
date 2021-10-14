7ad8765dcd46908200370606ec9edc4d9cd0c7d7
Quintero Agustin <agustin.quintero@limayrac.fr>
2021-10-14 16:23:33 +0200

    TRIGGER LIGNE FINITOT PIPO

--- storage/sql/FREDI_DATA.sql
+++ storage/sql/FREDI_DATA.sql
@@ -88,3 +88,23 @@ BEGIN
     SET NEW.mt_total = NEW.mt_repas + NEW.mt_peage + NEW.mt_hebergement+NEW.mt_km;
 END|
 
+DELIMITER |
+CREATE OR REPLACE TRIGGER after_update_ligne AFTER UPDATE
+ON ligne FOR EACH ROW
+BEGIN
+    DECLARE mt_total_ligne FLOAT;
+	SELECT SUM(ligne.mt_total) INTO mt_total_ligne FROM ligne, note WHERE ligne.id_note = NEW.id_note AND note.id_note = ligne.id_note;
+    UPDATE note SET mt_total = mt_total_ligne WHERE note.id_note = NEW.id_note;
+END|
+
+
+DELIMITER |
+CREATE OR REPLACE TRIGGER after_insert_ligne AFTER INSERT
+ON ligne FOR EACH ROW
+BEGIN
+    DECLARE mt_total_ligne FLOAT;
+	SELECT SUM(ligne.mt_total) INTO mt_total_ligne FROM ligne, note WHERE ligne.id_note = NEW.id_note AND note.id_note = ligne.id_note;
+    UPDATE note SET mt_total = mt_total_ligne WHERE note.id_note = NEW.id_note;
+END|
+
+
