593927c24e8027c33e6511918d5f729e73f9392b
david <david.peyrard@limayrac.fr>
2021-10-21 16:28:40 +0200

    correc

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # FREDI Groupe 3
 
-## Bienvenue sur le projet FREDI du groupe 3 qui se composent de :
+## Bienvenue sur le projet FREDI du groupe 3 qui se compose de :
 - Agustin ,
 - Yohan ,
 - David ,
@@ -10,11 +10,11 @@
 - **`/storage/livrables`**
 
 # Pour la mise en place du serveur SQL :
-- 1.Il vous faut vous rendre dans **/storage/sql**
-- 2.Puis ajoutez dans l'ordre les fichier suivant dans la partie SQL
+- 1.Il faut vous rendre dans **/storage/sql**
+- 2.Puis ajoutez dans l'ordre les fichiers suivants dans la partie SQL
     - `__fredi21_structure_simple.sql`
     - `__fredi21_trigger.sql`
-    - `__fredi21_data.sql` (Si vous voulez les données d'exemples)
+    - `__fredi21_data.sql` (Si vous voulez les données d'exemple)
 
 
 # Documentation détaillée du projet dans le dossier
@@ -37,9 +37,9 @@ mdp : Honore31000-/
 # TOUS LES LOGS SONT DANS
 - /logs/log.txt (les tentatives de connexions)
 
-# TOUS LES 'MAILS' SONT DANS DOSSIER /logs
+# TOUS LES 'MAILS' SONT DANS LE DOSSIER /logs
 ```
 Pour savoir quel fichier regarder il faut :
 1. regarder les fichiers avec le 'nom' => mail_
-2. il est suivit de [NOM D'UTILISATEUR]-DATE-H:M:S.txt
+2. il est suivi de [NOM D'UTILISATEUR]-DATE-H:M:S.txt
 ```
--- controleur.php
+++ controleur.php
@@ -6,5 +6,62 @@
   <h1>Menu Contrôleur</h1>
   <h2>Page de contrôle</h2>
 
+  <?php
+    
+    foreach($notes as $note) {
+    echo '<br><br><br><br><br>';
+    echo '<table>';
+    echo '<tr>';
+        echo '<th>ID Note</th>';
+        echo '<th>Validé ?</th>';
+        echo '<th>Montant Total</th>';
+        echo '<th>Date Remise</th>';
+        echo '<th>N° Ordre</th>';
+        echo '<th>ID Période</th>';
+    echo '</tr>';
+    echo '<tr>';
+        echo '<td>' . $note->get_id_note() . '</td>';
+        echo '<td>' . $note->get_est_valide() . '</td>';
+        echo '<td>' . $note->get_mt_total() . '</td>';
+        echo '<td>' . $note->get_dat_remise() . '</td>';
+        echo '<td>' . $note->get_nr_ordre() . '</td>';
+        echo '<td>' . $note->get_id_periode() . '</td>';
+    echo '</tr>';
+    echo '</table>';
+    foreach($note->get_lignes() as $ligne) {
+    echo '<table>';
+    echo '<tr>';
+        echo '<th>ID Ligne</th>';
+        echo '<th>Date Ligne</th>';
+        echo '<th>Libellé Trajet</th>';
+        echo '<th>Nb Km</th>';
+        echo '<th>Montant Km</th>';
+        echo '<th>Montant Péage</th>';
+        echo '<th>Montant Repas</th>';
+        echo '<th>Montant Hébergement</th>';
+        echo '<th>Montant Total</th>';
+        echo '<th>ID Motif</th>';
+        echo '<th>Modifier</th>';
+        echo '<th>Supprimer</th>';
+    echo '</tr>';
+    echo '<tr>';
+        echo '<td>' . $ligne->get_id_ligne() . '</td>';
+        echo '<td>' . $ligne->get_dat_ligne() . '</td>';
+        echo '<td>' . $ligne->lib_trajet() . '</td>';
+        echo '<td>' . $ligne->get_nb_km() . '</td>';
+        echo '<td>' . $ligne->get_mt_km() . '</td>';
+        echo '<td>' . $ligne->get_mt_peage() . '</td>';
+        echo '<td>' . $ligne->get_mt_repas() . '</td>';
+        echo '<td>' . $ligne->get_mt_hebergement() . '</td>';
+        echo '<td>' . $ligne->get_mt_total() . '</td>';
+        echo '<td>' . $ligne->get_id_motif() . '</td>';
+        echo '<td><a href="ligne_modifier.php?id_ligne='.$ligne->get_id_ligne().'">Modifier</a></td>';
+        echo '<td><a href="ligne_supprimer.php?id_ligne='.$ligne->get_id_ligne().'">Supprimer</a></td>';
+    }
+    echo '</tr>';
+    echo '</table>';
+  }
+  
 
+    ?>
 <?php include('footer.php'); ?>
