27a4bc56eb1934d2c07dc861ea60d3654a8bd209
Quintero Agustin <agustin.quintero@limayrac.fr>
2022-01-06 15:50:43 +0100

    json

--- app/dao/LigneDAO.php
+++ app/dao/LigneDAO.php
@@ -65,80 +65,109 @@
 
 
         /**
-    * Ajout d'une ligne
-    * @param \Ligne
-    * @return int Nombre de mises à jour
-    */
-    public function insert(ligne $ligne)
-    {
-        $sql = "INSERT INTO ligne(lib_trajet, nb_km, mt_peage, mt_repas, mt_hebergement, id_motif, id_note) 
-        values (:lib_trajet, :nb_km, :mt_peage, :mt_repas, :mt_hebergement, :id_motif, :id_note)";
-        $params = array(
-          ":lib_trajet" => $ligne->get_lib_trajet(),
-          ":nb_km" => $ligne->get_nb_km(),
-          ":mt_peage" => $ligne->get_mt_peage(),
-          ":mt_repas" => $ligne->get_mt_repas(),
-          ":mt_hebergement" => $ligne->get_mt_hebergement(),
-          ":id_motif" => $ligne->get_id_motif(),
-          ":id_note" => $ligne->get_id_note()
-        );
-        try {
-            $sth = $this->pdo->prepare($sql); // On passe par la méthode de la classe mère
-            $sth->execute($params);
-            $nb = $sth->rowcount();
-        } catch (PDOException $e) {
-            die("Erreur lors de la requête SQL : " . $e->getMessage());
-        }
-        return $nb;  // Retourne le nombre de mise à jour
-    } // insert()
-
-    /**
-    * Modification d'une ligne
-    * @param \ligne
-    * @return int Nombre de mises à jour
-    */
-    public function update(ligne $ligne)
-    {
-        $sql = "update ligne set lib_trajet=:lib_trajet,nb_km=:nb_km,mt_peage=:mt_peage,mt_repas=:mt_repas,mt_hebergement=:mt_hebergement,id_motif=:id_motif where id_ligne= :id_ligne";
-        $params = array(
-            ":id_ligne" => $ligne->get_id_ligne(),
+        * Ajout d'une ligne
+        * @param \Ligne
+        * @return int Nombre de mises à jour
+        */
+        public function insert(ligne $ligne)
+        {
+            $sql = "INSERT INTO ligne(lib_trajet, nb_km, mt_peage, mt_repas, mt_hebergement, id_motif, id_note) 
+            values (:lib_trajet, :nb_km, :mt_peage, :mt_repas, :mt_hebergement, :id_motif, :id_note)";
+            $params = array(
             ":lib_trajet" => $ligne->get_lib_trajet(),
             ":nb_km" => $ligne->get_nb_km(),
             ":mt_peage" => $ligne->get_mt_peage(),
             ":mt_repas" => $ligne->get_mt_repas(),
             ":mt_hebergement" => $ligne->get_mt_hebergement(),
-            ":id_motif" => $ligne->get_id_motif()
-        );
-        try {
-            $sth = $this->pdo->prepare($sql); // On passe par la méthode de la classe mère
-            $sth->execute($params);
-            $nb = $sth->rowCount();
-        } catch (PDOException $e) {
-            die("Erreur lors de la requête SQL : " . $e->getMessage());
-        }
-        return $nb;  // Retourne le nombre de mise à jour
-    } // update()
+            ":id_motif" => $ligne->get_id_motif(),
+            ":id_note" => $ligne->get_id_note()
+            );
+            try {
+                $sth = $this->pdo->prepare($sql); // On passe par la méthode de la classe mère
+                $sth->execute($params);
+                $nb = $sth->rowcount();
+            } catch (PDOException $e) {
+                die("Erreur lors de la requête SQL : " . $e->getMessage());
+            }
+            return $nb;  // Retourne le nombre de mise à jour
+        } // insert()
 
-    /**
-    * Suppression d'une ligne
-    * @param int ID de la ligne
-    * @return int Nombre de mises à jour
-    */
-    public function delete($id_ligne)
-    {
-        $sql = "DELETE FROM ligne WHERE id_ligne= :id_ligne";
-        $params = array(
-        ":id_ligne" => $id_ligne
-        );
-        try {
-            $sth = $this->pdo->prepare($sql);
-            $sth->execute($params); // On passe par la méthode de la classe mère
-            $nb = $sth->rowCount();
-        } catch (PDOException $e) {
-            die("Erreur lors de la requête SQL : " . $e->getMessage());
+        /**
+        * Modification d'une ligne
+        * @param \ligne
+        * @return int Nombre de mises à jour
+        */
+        public function update(ligne $ligne)
+        {
+            $sql = "update ligne set lib_trajet=:lib_trajet,nb_km=:nb_km,mt_peage=:mt_peage,mt_repas=:mt_repas,mt_hebergement=:mt_hebergement,id_motif=:id_motif where id_ligne= :id_ligne";
+            $params = array(
+                ":id_ligne" => $ligne->get_id_ligne(),
+                ":lib_trajet" => $ligne->get_lib_trajet(),
+                ":nb_km" => $ligne->get_nb_km(),
+                ":mt_peage" => $ligne->get_mt_peage(),
+                ":mt_repas" => $ligne->get_mt_repas(),
+                ":mt_hebergement" => $ligne->get_mt_hebergement(),
+                ":id_motif" => $ligne->get_id_motif()
+            );
+            try {
+                $sth = $this->pdo->prepare($sql); // On passe par la méthode de la classe mère
+                $sth->execute($params);
+                $nb = $sth->rowCount();
+            } catch (PDOException $e) {
+                die("Erreur lors de la requête SQL : " . $e->getMessage());
+            }
+            return $nb;  // Retourne le nombre de mise à jour
+        } // update()
+
+        /**
+        * Suppression d'une ligne
+        * @param int ID de la ligne
+        * @return int Nombre de mises à jour
+        */
+        public function delete($id_ligne)
+        {
+            $sql = "DELETE FROM ligne WHERE id_ligne= :id_ligne";
+            $params = array(
+            ":id_ligne" => $id_ligne
+            );
+            try {
+                $sth = $this->pdo->prepare($sql);
+                $sth->execute($params); // On passe par la méthode de la classe mère
+                $nb = $sth->rowCount();
+            } catch (PDOException $e) {
+                die("Erreur lors de la requête SQL : " . $e->getMessage());
+            }
+            return $nb;  // Retourne le nombre de mise à jour
         }
-        return $nb;  // Retourne le nombre de mise à jour
-    }
+
+        /**
+        * Lecture de toutes les lignes d'un ID note
+        * @param int $id_note
+        * @return \Ligne
+        * @throws Exception
+        */
+        function findAllByIdUser($id_user, $id_periode) {
+            $sql = "SELECT * FROM ligne, note
+            WHERE ligne.id_note = note.id_note 
+            AND note.id_utilisateur = :id_utilisateur
+            AND note.id_periode = :id_periode";
+            try {
+                $sth = $this->pdo->prepare($sql);
+                $sth->execute(array(
+                    ":id_utilisateur" => $id_user,
+                    ":id_periode" => $id_periode
+                ));
+                $rows = $sth->fetchAll(PDO::FETCH_ASSOC);
+            } catch (PDOException $e) {
+                die("Erreur lors de la requête SQL : " . $e->getMessage());
+            }
+            $lignes = array();
+            foreach ($rows as $row) {
+                $lignes[] = new Ligne($row);
+            }
+            // Retourne un tableau d'objets
+            return $lignes;
+        } //findAllByIdNote()
 
     }
 ?>
